dd770e6c7fb193bc5051697b8d4369a5cc2f5390
Daniel Rugg <dfrugg@gmail.com>
2025-03-21 17:24:00 -0400

    Fixing dependency range issues

--- project.clj
+++ project.clj
@@ -7,6 +7,17 @@
 
   :test-paths ["test/clj"]
 
+  :managed-dependencies [[javax.annotation/javax.annotation-api "1.3.2"]
+                         [org.jetbrains/annotations "20.0.0"]
+                         [com.fasterxml.jackson.core/jackson-core "2.14.3"]
+                         [com.fasterxml.jackson.core/jackson-databind "2.14.3"]
+                         [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.14.3"]
+                         [software.amazon.jsii/jsii-runtime "1.99.0"]
+                         [software.amazon.awscdk/cdk-asset-awscli-v1 "2.2.202"]
+                         [software.amazon.awscdk/cdk-asset-kubectl-v20 "2.1.2"]
+                         [software.amazon.awscdk/cdk-asset-node-proxy-agent-v6 "2.0.3"]
+                         [software.constructs/constructs "10.3.0"]]
+
   :dependencies [[org.clojure/clojure "1.11.3"]
                  [environ "1.2.0"]
                  [software.amazon.awscdk/aws-cdk-lib "2.144.0"]]
